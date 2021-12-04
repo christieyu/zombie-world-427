639d3da4c38db7dc5e010122bd2d9c8ef423a215
Christie Yu <43098796+christieyu@users.noreply.github.com>
2021-12-04 12:50:00 -0500

    updated zombie controllers

--- controllers/purple_zombie/purple_zombie.py
+++ controllers/purple_zombie/purple_zombie.py
@@ -151,10 +151,10 @@ class Pedestrian (Supervisor):
         while not self.step(self.time_step) == -1:
             self.translation = self.translationField.getSFVec3f()
             self.move_zombie(self.translation[0], self.translation[2], goal[0],goal[1])
-            
+            emitter.send(message)
             if (timer == 32): #only change movement once every second
                 timer = 0
-                emitter.send(message)
+                #emitter.send(message)
                 youbotTranslation = youbotTranslationField.getSFVec3f()
 
                 berryDistance = self.check_berry_close(self.translation)
